c5ccbe9bdaf71164331064155614805fd775b327
Joshua Kirts <57331915+JoshuaKirts@users.noreply.github.com>
2023-05-07 18:28:24 -0700

    Delete components directory

--- components/scripts/intro/jquery.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var letters = $('h1').text();
-    var nHTML = '';
-
-    for (var letter of letters) {
-      nHTML += "<span class='intro' aria-hidden='true' style='opacity: 1;'>" + letter + "</span>";
-    }
-    $('h1').html(nHTML);
-  })
